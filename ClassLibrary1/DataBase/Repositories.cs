b506cc7f7481e08785c7d523b325c621505d15db
DogukanAgar24 <dogukanagar154@gmail.com>
2026-04-27 22:25:08 +0300

    SqlLite bağlantısına başladım

--- ClassLibrary1/DataBase/Repositories.cs
+++ ClassLibrary1/DataBase/Repositories.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Data.Sqlite;
 
 namespace WorkoutControlling.DataBase
 {
-	internal class Repositories
+	public class Repositories
 	{
+		private readonly string _dbPath= "workoutControlling.db";
+
+		public Repositories()
+		{
+			InitializeDataBase();
+		}
+
+		private void InitializeDataBase()
+		{
+			using var connection = new SqliteConnection($"Data Source={_dbPath}");
+			connection.Open();
+
+			string createExercisesTable=@"CREATE TABLE IF NOT EXISTS Exercises (
+										Id INTEGER PRIMARY KEY AUTOINCREMENT,
+										Name TEXT NOT NULL,
+										PrimaryMuscle TEXT NOT NULL
+									);";
+
+			using var createTableCommand = new SqliteCommand(createExercisesTable, connection);
+			createTableCommand.ExecuteNonQuery();
+		}
 	}
 }
